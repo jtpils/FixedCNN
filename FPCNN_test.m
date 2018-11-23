8fe15bebeada4f1c7c1e63a345e59168c002a33b
JackGittes <1554146770@qq.com>
2018-11-24 01:37:35 +0800

    update pooling
    the pooling channel-wise operation changed to tensor-wise, which means a great acceleration for pooling

--- FPCNN_test.m
+++ FPCNN_test.m
@@ -1,15 +1,15 @@
 clc;clear all;
 f = fimath('CastBeforeSum', 0, 'OverflowMode', 'Saturate', 'RoundMode', 'floor', ... 
-'ProductMode', 'SpecifyPrecision', 'SumMode', 'SpecifyPrecision', 'ProductWordLength', 16, ...
-'ProductFractionLength', 8, 'SumWordLength', 16, 'SumFractionLength', 8);
-t = numerictype('WordLength', 16, 'FractionLength', 8);
+'ProductMode', 'SpecifyPrecision', 'SumMode', 'SpecifyPrecision', 'ProductWordLength', 32, ...
+'ProductFractionLength',16, 'SumWordLength', 32, 'SumFractionLength', 16);
+t = numerictype('WordLength', 32, 'FractionLength',16);
 roundm = 'floor';
 
 img = load('test_img\mat_img\img.mat');
 cnn_par = load('model.mat');
 net = cnn_par.net;
 profile on
-for i=1:20
+for i=1:50
     layer_res = FixConvNet(img.img1,net,t,f,roundm);
 end
 profile viewer
--- Pooling.m
+++ Pooling.m
@@ -35,10 +35,32 @@ function res = PoolingByType(im,t,f,window_shape,stride,pad_method,pool_func)
     if strcmp(pad_method,'SAME')
         [im,channel_size] = PoolPadding(im,channel_size,im_d,out_size,stride,window_shape,t,f);
     end
-    res = fi(zeros([out_size,im_d]),t,f);    
-    for i=1:im_d
-        res(:,:,i) = PoolingOneChannel(im(:,:,i),channel_size,out_size,window_shape,stride,pool_func);
+    
+    res = PoolingTensor(im,im_d,channel_size,out_size,window_shape,stride,pool_func);
+end
+
+function [res,pad_channel_size] = PoolPadding(im,channel_size,im_d,out_size,stride,window_shape,t,f)
+    pad_channel_size = (out_size-1).*stride+window_shape;
+    res = fi(zeros([pad_channel_size,im_d]),t,f);
+    res(1:channel_size(1),1:channel_size(2),:) = im;
+end
+
+function res = PoolingTensor(im,im_d,channel_size,out_size,window_shape,stride,pool_func)
+    pool_len = channel_size-window_shape+1;
+    
+    pos_one_col = repmat([1:stride(1):pool_len(1)],[out_size(1),1]);
+    gap_every_col = repmat(stride(2)*pool_len(1)*[0:out_size(2)-1]',[1,out_size(1)]);
+    pos = pos_one_col+gap_every_col;
+      
+    tmp = im2col(reshape([1:prod(channel_size)],channel_size),window_shape,'sliding');
+    
+    tmp = tmp(:,pos(:));
+    tmp1 = zeros(prod(window_shape),prod(out_size),im_d);
+    for i =1:im_d
+        tmp1(:,:,i)=(i-1)*prod(channel_size)+tmp;
     end
+    
+    res = permute(reshape(pool_func(im(tmp1)),[out_size,im_d]),[2,1,3]);
 end
 
 function res = PoolingOneChannel(im,channel_size,out_size,window_shape,stride,pool_func)
@@ -51,10 +73,4 @@ function res = PoolingOneChannel(im,channel_size,out_size,window_shape,stride,po
     tmp = im2col(reshape([1:prod(channel_size)],channel_size),window_shape,'sliding');
     tmp = tmp(:,pos(:));
     res = reshape(pool_func(im(tmp)),out_size)';
-end
-
-function [res,pad_channel_size] = PoolPadding(im,channel_size,im_d,out_size,stride,window_shape,t,f)
-    pad_channel_size = (out_size-1).*stride+window_shape;
-    res = fi(zeros([pad_channel_size,im_d]),t,f);
-    res(1:channel_size(1),1:channel_size(2),:) = im;
 end
